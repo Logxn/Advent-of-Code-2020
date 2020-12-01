1b2b62fa9129e1c2312937e0fad8848a0f8141e0
Logan <hello@loganthompson.de>
2020-12-01 18:00:30 +0100

    Added comments

--- 01 - Day One/solution.py
+++ 01 - Day One/solution.py
@@ -1,14 +1,23 @@
+# Advent of Code 2020 - Day 1
+# Task 1: Find two numbers that give 2020 if you add them up.
+#         Then multiply them to get the solution for this task.
+# Task 2: Find three number that give 2020 if you add them up.
+#         Then multiply them to get the solution for this task.
+#
+# Note: This solution is quick and there might be better solutions. But it works.
+
 f = open("input.txt", "r")
 input = f.read().split("\n")
-numbers = list(map(int, input))
+numbers = list(map(int, input)) # Convert the list of string numbers to a list of int numbers
 
+# Task 1
 for i in numbers:
     for j in numbers:
         if i + j == 2020:
             solution1 = i * j
             break
 
-
+# Task 2
 for i in numbers:
     for j in numbers:
         for x in numbers:
