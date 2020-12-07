9e4a6bea57dfb79700c4479d61b3b098816bd559
Logan <hello@loganthompson.de>
2020-12-08 00:58:42 +0100

    Finally Day 6 Pt. 2

--- 06 - Day Six/solution.py
+++ 06 - Day Six/solution.py
@@ -7,13 +7,29 @@ groups = f.read().split("\n\n")
 def removeDupWithOrder(str):  
     return "".join(OrderedDict.fromkeys(str))
 
+def duplicate_count(s):
+    if len(s) == 1:
+        return 1
+    else:
+        return len([x for x in set(s) if s.count(x) > 1])
+
+def duplicate_raw():
+    solution = 0
+    for group in groups:
+        answers = group.split("\n")
+        
+        intersections = set(answers[0]).intersection(*answers)
+        solution += len(intersections)
+    return solution
+
 solution = 0
 
 for group in groups:
     group = group.replace("\n", "")
+    duplicates = duplicate_count(group)
     answer = removeDupWithOrder(group)
 
     solution += len(answer)
 
-
 print(solution)
+print(duplicate_raw())
